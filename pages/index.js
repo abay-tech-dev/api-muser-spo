d8c2a33f4690785bc61af399d7f5d4fead6432a0
antho <a.bay@nicomatic.fr>
2025-06-27 09:37:12 +0200

    12px

--- pages/index.js
+++ pages/index.js
@@ -108,7 +108,7 @@ export default function Home() {
           backgroundImage: `url(${coverUrl})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
-          filter: "blur(150px)",
+          filter: "blur(12px)",
         }} />
       )}
       {/* NOIR OPACITÉ MOINS FORTE */}
@@ -116,7 +116,7 @@ export default function Home() {
         position: "absolute",
         inset: 0,
         zIndex: 2,
-        background: "rgba(0,0,0,0.2)",
+        background: "rgba(0,0,0,0.05)",
         transition: "background 0.2s"
       }} />
 
